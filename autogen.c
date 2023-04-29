5235355007eb497b438213ebf8820d834cabbde0
mg-2018 <alsrl6710@gmail.com>
2023-04-29 20:19:10 +0900

    Add example midi file

--- autogen.c
+++ autogen.c
@@ -4,48 +4,48 @@
 #define PPQN 480
 
 int main(void) {
-	FILE *out = fopen("autogen.mid", "wb");
-	int32_t trackSize = 0;
-	uint8_t trackByte[4] = {0};
-	
-	WriteMidiHeader(PPQN, out);
-	WriteTrackHeader(out);
-	for(int i=0; i<4; i++) {
-		fwrite(trackByte+i, sizeof(uint8_t), 1, out);
-	}
-	
-	trackSize += WriteTrackName(out);
-	trackSize += WriteSysEx_GS_Reset(out);
-	trackSize += WriteControlChange(0, 9, 7, 100, out);
-	trackSize += WriteControlChange(0, 9, 0, 0, out);
-	trackSize += WriteControlChange(0, 9, 32, 0, out);
-	trackSize += WriteProgramChange(0, 9, 80, out);
-	trackSize += WriteTempo(0, 120, out);
-	
-	int offset = 0;
-	bool add = true;
-	srand(time(NULL));
-	for(int i=0; i<256; i++) {
-		offset = rand()%25;
-		trackSize += WriteNote(9, 0, 35+offset, 100, out);
-		
-		trackSize += WriteNote(9, PPQN/4, 35+offset, 0, out);
-		
-		/*
-		if(offset == 16) add = false;
-		else if(offset == 0) add = true;
-		offset += (add) ? 1 : -1;
-		*/
-	}
-	trackSize += WriteTrackEnd(out);
-	
-	GetBytes(trackByte, trackSize, 4);
-	fseek(out, 18, SEEK_SET);
-	for(int i=0; i<4; i++) {
-		fwrite(trackByte+i, sizeof(uint8_t), 1, out);
-	}
-	fclose(out);
-	
-	return 0;
+    FILE *out = fopen("autogen.mid", "wb");
+    int32_t trackSize = 0;
+    uint8_t trackByte[4] = {0};
+    
+    WriteMidiHeader(PPQN, out);
+    WriteTrackHeader(out);
+    for(int i=0; i<4; i++) {
+        fwrite(trackByte+i, sizeof(uint8_t), 1, out);
+    }
+    
+    trackSize += WriteTrackName(out);
+    trackSize += WriteSysEx_GS_Reset(out);
+    trackSize += WriteControlChange(0, 9, 7, 100, out);
+    trackSize += WriteControlChange(0, 9, 0, 0, out);
+    trackSize += WriteControlChange(0, 9, 32, 0, out);
+    trackSize += WriteProgramChange(0, 9, 80, out);
+    trackSize += WriteTempo(0, 120, out);
+    
+    int offset = 0;
+    bool add = true;
+    srand(time(NULL));
+    for(int i=0; i<256; i++) {
+        offset = rand()%25;
+        trackSize += WriteNote(9, 0, 35+offset, 100, out);
+        
+        trackSize += WriteNote(9, PPQN/4, 35+offset, 0, out);
+        
+        /*
+        if(offset == 16) add = false;
+        else if(offset == 0) add = true;
+        offset += (add) ? 1 : -1;
+        */
+    }
+    trackSize += WriteTrackEnd(out);
+    
+    GetBytes(trackByte, trackSize, 4);
+    fseek(out, 18, SEEK_SET);
+    for(int i=0; i<4; i++) {
+        fwrite(trackByte+i, sizeof(uint8_t), 1, out);
+    }
+    fclose(out);
+    
+    return 0;
 }
 
--- parser.h
+++ parser.h
@@ -20,87 +20,87 @@
 #define NOTE_TOO_LONG -3
 
 int ReadSingleNote(FILE *fin, char *buf) {
-	int pos = 0;
-	memset(buf, 0, sizeof(char)*4);
-	while(true) {
-		if(pos > 4) {
-			return NOTE_TOO_LONG;
-		}
-		
-		char ch = fgetc(fin);
-		if(ch == -1 || isspace(ch)) {
-			return SUCCESS;
-		}
-		
-		else {
-			buf[pos] = ch;
-		}
-		pos++;
-	}
+    int pos = 0;
+    memset(buf, 0, sizeof(char)*4);
+    while(true) {
+        if(pos > 4) {
+            return NOTE_TOO_LONG;
+        }
+        
+        char ch = fgetc(fin);
+        if(ch == -1 || isspace(ch)) {
+            return SUCCESS;
+        }
+        
+        else {
+            buf[pos] = ch;
+        }
+        pos++;
+    }
 }
 
 int ParseNoteInfo(char *buf) {
-	int noteNum;
-	int parsePos = 0;
-	
-	switch(buf[parsePos]) {
-		case 'C':
-		case 'c':
-			noteNum = 0; break;
-		
-		case 'D':
-		case 'd':
-			noteNum = 2; break;
-			
-		case 'E':
-		case 'e':
-			noteNum = 4; break;
-			
-		case 'F':
-		case 'f':
-			noteNum = 5; break;
-		
-		case 'G':
-		case 'g':
-			noteNum = 7; break;
-		
-		case 'A':
-		case 'a':
-			noteNum = 9; break;
-		
-		case 'B':
-		case 'b':
-			noteNum = 11; break;
-		
-		case 'R':
-		case 'r':
-			return 127;
-		
-		default:
-			return INVALID_CHAR;
-	}
-	
-	parsePos++;
-	if(buf[parsePos] == '#') {
-		noteNum++;
-		parsePos++;
-	}
-	
-	else if(!isdigit(buf[parsePos])) {
-		return INVALID_CHAR;
-	}
-	
-	if(isdigit(buf[parsePos])) {
-		char *temp = buf+parsePos;
-		if(*(temp+1) != 0) {
-			return NOTE_OVERFLOW;
-		}
-		
-		else {
-			noteNum += (*temp-'0') * 12;
-		}
-	}
-	
-	return noteNum;
+    int noteNum;
+    int parsePos = 0;
+    
+    switch(buf[parsePos]) {
+        case 'C':
+        case 'c':
+            noteNum = 0; break;
+        
+        case 'D':
+        case 'd':
+            noteNum = 2; break;
+            
+        case 'E':
+        case 'e':
+            noteNum = 4; break;
+            
+        case 'F':
+        case 'f':
+            noteNum = 5; break;
+        
+        case 'G':
+        case 'g':
+            noteNum = 7; break;
+        
+        case 'A':
+        case 'a':
+            noteNum = 9; break;
+        
+        case 'B':
+        case 'b':
+            noteNum = 11; break;
+        
+        case 'R':
+        case 'r':
+            return 127;
+        
+        default:
+            return INVALID_CHAR;
+    }
+    
+    parsePos++;
+    if(buf[parsePos] == '#') {
+        noteNum++;
+        parsePos++;
+    }
+    
+    else if(!isdigit(buf[parsePos])) {
+        return INVALID_CHAR;
+    }
+    
+    if(isdigit(buf[parsePos])) {
+        char *temp = buf+parsePos;
+        if(*(temp+1) != 0) {
+            return NOTE_OVERFLOW;
+        }
+        
+        else {
+            noteNum += (*temp-'0') * 12;
+        }
+    }
+    
+    return noteNum;
 }
 
